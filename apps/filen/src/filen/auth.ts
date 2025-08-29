1f674cb5b4577c7deadebcd6e917306441b34a9b
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2025-08-29 21:09:57 +0700

    update generate totp

--- apps/filen/src/filen/auth.ts
+++ apps/filen/src/filen/auth.ts
@@ -2,6 +2,9 @@ import { TOTP } from "totp-generator";
 import { env } from "../utils/env.js";
 
 export const generateTOTP = () => {
-  const result = TOTP.generate(env.FILEN_2FA);
+  const result = TOTP.generate(env.FILEN_2FA, {
+    algorithm: "SHA-1",
+    digits: 6,
+  });
   return result.otp;
 };
