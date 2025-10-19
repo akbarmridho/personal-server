673215e4b690c2aea828e38c9c2987dc120492ae
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2025-10-19 11:34:34 +0700

    feat: marketdetector

--- apps/kb-backend/src/stock/stockbit/market-detector.ts
+++ apps/kb-backend/src/stock/stockbit/market-detector.ts
@@ -1,1 +1,53 @@
-// todo
+import axios from "axios";
+import dayjs from "dayjs";
+import { KV } from "../../db/kv.js";
+import type { JsonValue } from "../../db/types.js";
+import {
+  type BaseStockbitResponse,
+  StockbitAuthError,
+  stockbitAuth,
+} from "./auth.js";
+
+export const getMarketDetector = async (input: {
+  ticker: string;
+  from: Date;
+  to: Date;
+}) => {
+  const fromFormatted = dayjs(input.from).format("YYYY-MM-DD");
+  const toFormatted = dayjs(input.to).format("YYYY-MM-DD");
+
+  const rawData = await KV.getOrSet(
+    `stockbit.marketdetector.${input.ticker}.${fromFormatted}.${toFormatted}`,
+    async () => {
+      const authData = await stockbitAuth.get();
+
+      if (!authData) {
+        throw new StockbitAuthError("Stockbit auth not found");
+      }
+
+      const response = await axios.get(
+        `https://exodus.stockbit.com/marketdetectors/${input.ticker}?from=${fromFormatted}&to=${toFormatted}&transaction_type=TRANSACTION_TYPE_NET&market_board=MARKET_BOARD_REGULER&investor_type=INVESTOR_TYPE_ALL&limit=25`,
+        {
+          headers: {
+            Authorization: `Bearer ${authData.accessToken}`,
+          },
+        },
+      );
+
+      const data = response.data;
+
+      return data as JsonValue;
+    },
+    dayjs().add(3, "hour").toDate(),
+    true,
+  );
+
+  const data = rawData as any as BaseStockbitResponse<{
+    bandar_detector: Record<string, any>;
+    broker_summary: Record<string, any>;
+    from: string;
+    to: string;
+  }>;
+
+  return data.data;
+};
