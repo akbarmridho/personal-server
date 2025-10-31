8b28de5df6b4cf7c92683d6c2eab84fc4005f625
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2025-10-31 09:20:48 +0700

    add todo

---
+++ apps/kb-backend/src/stock/stockbit/stream.ts
@@ -0,0 +1,5 @@
+// di stream
+// https://stockbit.com/symbol/BBRI
+// tab news
+// reports juga kalau bisa download pdfnya dan bisa reason on the file better tapi gw gak tau bagusnya gmn
+// terus pinned post
