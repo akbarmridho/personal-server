142f2ca811af56afb2aca5461af9ed70ebef4283
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2025-12-26 19:43:17 +0700

    simplify ihsg overview

--- apps/kb-backend/src/stock/endpoints/ihsg/overview.ts
+++ apps/kb-backend/src/stock/endpoints/ihsg/overview.ts
@@ -1,17 +1,9 @@
 import dayjs from "dayjs";
 import { getChartbitData } from "../../stockbit/chartbit.js";
 import { getEmittenInfo } from "../../stockbit/emitten-info.js";
-import { getStockSeasonality } from "../../stockbit/seasonality.js";
 import {
-  calculateADX,
-  calculateATR,
-  calculateBBANDS,
-  calculateIchimoku,
-  calculateMACD,
-  calculateOBV,
   calculateRSI,
   calculateSMA,
-  calculateVolumeProfile,
   calculateZigZag,
   downsampleToWeekly,
   scanForRecentPatterns,
@@ -67,20 +59,9 @@ export const getIHSGOverview = async () => {
       trend: {
         sma50: calculateSMA(chartbit, 50),
         sma200: calculateSMA(chartbit, 200),
-        ichimoku_cloud: calculateIchimoku(chartbit),
       },
       momentum: {
-        adx14: calculateADX(chartbit, 14),
-        macd_12_26_9: calculateMACD(chartbit, 12, 26, 9),
         rsi14: calculateRSI(chartbit, 14),
-      },
-      volume: {
-        obv: calculateOBV(chartbit),
-        volume_profile_3m: calculateVolumeProfile(chartbit, "3m"),
-      },
-      volatility: {
-        atr14: calculateATR(chartbit, 14),
-        bollinger_bands_20_2: calculateBBANDS(chartbit),
       },
     },
     candlestick_patterns: scanForRecentPatterns(chartbit),
